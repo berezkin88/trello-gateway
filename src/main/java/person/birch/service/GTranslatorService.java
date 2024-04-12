1135dbbff28926b53013d4cde65f1c3faabdb231
berezkin88 <berezkin88@ukr.net>
2024-04-12 21:42:28 +0300

    adjusted to quarkus usage

--- src/main/java/person/birch/service/GTranslatorService.java
+++ src/main/java/person/birch/service/GTranslatorService.java
@@ -1,37 +1,37 @@
 package person.birch.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import jakarta.inject.Singleton;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-@Service
+@Singleton
 public class GTranslatorService {
     private static final String USER_AGENT = "User-Agent";
     private static final String MOZILLA_5_0 = "Mozilla/5.0";
-    @Value("${g-translator.url}")
-    private String url;
+    @ConfigProperty(name = "g-translator.url")
+    String url;
 
     /**
      * Translate text from ukrainian to english
      * @param text string in ukrainian
      * @return english text
-     * @throws IOException
      */
-    public String translate(String text) throws IOException {
+    public String translate(String text) throws IOException, URISyntaxException {
         String encodedQuery = URLEncoder.encode(text, StandardCharsets.UTF_8);
         String urlStr = url + "?q=" + encodedQuery + "&target=en&source=uk";
-        URL url = new URL(urlStr);
-        StringBuilder response = new StringBuilder();
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        var completeUrl = new URI(urlStr).toURL();
+        var response = new StringBuilder();
+        var con = (HttpURLConnection) completeUrl.openConnection();
         con.setRequestProperty(USER_AGENT, MOZILLA_5_0);
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        var in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
